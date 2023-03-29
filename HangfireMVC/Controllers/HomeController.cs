ef859335acca39a3ffcfcb24bfa56fd88c7909f5
intel0620 <intel0620@gmail.com>
2023-03-29 16:27:51 +0800

    #test

--- HangfireMVC/Controllers/HomeController.cs
+++ HangfireMVC/Controllers/HomeController.cs
@@ -23,20 +23,23 @@ namespace HangfireMVC.Controllers
             //    , Cron.Minutely);
 
             // 每天台灣時間下午3點39分執行
-            RecurringJob.AddOrUpdate(() => new Jobs().CreateFile(), "39 15 * * *" , TimeZoneInfo.Local);
-
+        //    RecurringJob.AddOrUpdate(() => new Jobs().CreateFile(), "39 15 * * *" , TimeZoneInfo.Local);
 
             // 每天台灣時間下午3點40分執行
             RecurringJob.AddOrUpdate(() => new Jobs().CreateFile2(), "40 15 * * *", TimeZoneInfo.Local);
 
-
             //每天上午12點與下午12點各執行一次
-            RecurringJob.AddOrUpdate(() => new Jobs().CreateFile(), "0 0,12 * * *", TimeZoneInfo.Local);
+       //     RecurringJob.AddOrUpdate(() => new Jobs().CreateFile(), "0 0,12 * * *", TimeZoneInfo.Local);
 
             //每天上午5點  上午11點  下午15點  下午22點各執行一次
             RecurringJob.AddOrUpdate(() => new Jobs().CreateFile(), "0 5,11,15,22 * * *", TimeZoneInfo.Local);
 
-            return View();
+
+            // 每天台灣時間下午3點10分執行,任務名稱是 ABC
+            RecurringJob.AddOrUpdate("ABC", () => new Jobs().CreateFile(), "10 15 * * *", TimeZoneInfo.Local);
+
+            //導向 http://loclahost/hangfire
+            return Redirect("/hangfire");
         }
 
         public ActionResult About()
